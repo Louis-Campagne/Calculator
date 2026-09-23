e5dd7d8199d3d62f09da642daf6a280bba99a16d
Louis Campagne <lcampagne001@iutbayonne;univ-pau.fr>
2026-09-23 16:28:39 +0200

    9.ecEiEcrire des tests paramétrés

--- src/test/java/com/campagne/calculator/CalculatorTest.java
+++ src/test/java/com/campagne/calculator/CalculatorTest.java
@@ -3,6 +3,9 @@ package com.campagne.calculator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -30,6 +33,23 @@ public class CalculatorTest {
         //THEN
         assertThat(somme).isEqualTo(3);
     }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,  1,  1",
+            "1,  2,  3",
+            "-2, 2,  0",
+            "0,  0,  0",
+            "-1, -2, -3"
+    })
+    void add_devrait_calculer_la_somme_pour_plusieurs_valeurs(int opG, int opD, int resultatAttendu) {
+        // WHEN
+        int somme = Calculator.add(opG, opD);
+
+        // THEN
+        assertThat(somme).isEqualTo(resultatAttendu);
+    }
+
 
     @Test
     void divide_devrait_calculer_la_division_de_deux_entier(){
