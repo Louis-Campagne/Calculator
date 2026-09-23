2ee92b66568fb98687dfa316f0885addf9d77b3c
Louis Campagne <lcampagne001@iutbayonne;univ-pau.fr>
2026-09-23 16:15:45 +0200

    7.Simplification d'un test

--- src/main/java/com/campagne/calculator/Calculator.java
+++ src/main/java/com/campagne/calculator/Calculator.java
@@ -1,4 +1,12 @@
 package com.campagne.calculator;
 
 public class Calculator {
+    public static int add(int opG1, int opG2) {
+        return 1;
+    }
+
+
+    public static int divide(int opG1, int opG2) {
+        return 1;
+    }
 }
--- src/test/java/com/campagne/calculator/CalculatorTest.java
+++ src/test/java/com/campagne/calculator/CalculatorTest.java
@@ -1,4 +1,45 @@
 package com.campagne.calculator;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculator = null;
+    }
+
+    @Test
+    void add_devrait_calculer_la_somme_de_deux_entier() {
+        //GIVEN
+        int opG1 = 1;
+        int opG2 = 2;
+
+        //WHEN
+        int somme = Calculator.add(opG1, opG2);
+        //THEN
+        assertThat(somme).isEqualTo(3);
+    }
+
+    @Test
+    void divide_devrait_calculer_la_division_de_deux_entier(){
+        //GIVEN
+        int opG1 = 4;
+        int opG2 = 2;
+
+        //WHEN
+        int division = Calculator. divide(opG1, opG2);
+        //THEN
+        assertThat(division).isEqualTo(2);
+    }
 }
